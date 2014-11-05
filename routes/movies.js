7034a59ccc3e8468da4447602b08fc0b60640f03
John Stewart <lead.j2ee.engineer@gmail.com>
2014-11-04 17:56:30 -0700

    filter the movie showtime API output to what I want for the angular UI

--- routes/movies.js
+++ routes/movies.js
@@ -5,6 +5,13 @@ var RestClient = require('node-rest-client').Client;
 var apiKey = 'fbagxybtcezrfq7bzzbf5yat';
 var zip = '80134';
 
+var outputFilter = function(movie) {
+  return {
+    "title": movie.title, "rating": (movie.ratings || [{code: 'n/a'}])[0].code, "releaseDate": movie.releaseDate, 
+    "showtimes": movie.showtimes.map(function(st) { return {theatre: st.theatre.name, time: st.dateTime} } )
+  }
+}
+
 
 /* GET movies listing. */
 router.get('/', function(req, res) {
@@ -20,16 +27,17 @@ router.get('/', function(req, res) {
     args, 
     function(data, response) {
       // parsed response body as js object
-      console.log(data);
+      // return a filtered subset of the api data
+      res.json(data.map(outputFilter));
   });
 
-  res.json({"title": "Fury", 
-         "shortDescription": "A tank commander takes his men on a deadly mission to strike at the heart of Nazi Germany.",
-         "rating": "R",    
-         "releaseDate": "2014-10-17",
-         "showtimes": [{ "theatre": "AMC Twenty Mile 10", "time" : "2014-11-04T13:05"},
-                       { "theatre": "AMC Twenty Mile 10", "time" : "2014-11-04T13:05"}]
-         });
+  // res.json({"title": "Fury", 
+  //        "shortDescription": "A tank commander takes his men on a deadly mission to strike at the heart of Nazi Germany.",
+  //        "rating": "R",    
+  //        "releaseDate": "2014-10-17",
+  //        "showtimes": [{ "theatre": "AMC Twenty Mile 10", "time" : "2014-11-04T13:05"},
+  //                      { "theatre": "AMC Twenty Mile 10", "time" : "2014-11-04T13:05"}]
+  //        });
 });
 
 
