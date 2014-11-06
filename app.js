60a5876708f55154a45016be403f4911897958c8
John Stewart <lead.j2ee.engineer@gmail.com>
2014-11-06 16:17:09 -0700

    deleting sass compile because heroku barfs

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var sass = require('node-sass-middleware');
 var routes = require('./routes/index');
 var movies = require('./routes/movies');
 
@@ -14,21 +12,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(
-  sass({
-    src: __dirname + '/public',
-    dest: __dirname + '/public',
-    debug: true,
-    outputStyle: 'compressed'
-  })
-);
+// if (app.get('env') === 'development') {
+//     var sass = require('node-sass-middleware');
+//     app.use(
+//       sass({
+//         src: __dirname + '/public',
+//         dest: __dirname + '/public',
+//         debug: true,
+//         outputStyle: 'compressed'
+//       })
+//     );
+// }
 
 app.use(express.static(path.join(__dirname, 'public')));
 
